f453fd4a79fc8cdf4ea62c43ab0e251417daae51
Ahmed <201700684@student.polytechnic.bh>
2020-11-14 16:48:14 +0300

    hello

---
+++ Git Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Tutorial
+//
+//  Created by mobileProg on 11/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello world")
+    }
+
+    
+}
+
